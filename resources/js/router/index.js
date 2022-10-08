f2b682cc4e28fe11e9b81dc76553bda3b8be90e0
lamviet thinh <lamvietthinh048@gmail.com>
2022-10-08 09:02:24 +0700

    login frontend

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -23,6 +23,14 @@ router.beforeEach((to, from, next) => {
     }
     const { middleware } = to.meta;
     const context = { to, from, next };
+    const publicPages = ['/login', '/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+
+    if (authRequired && !loggedIn) {
+        return next('/admin/login');
+    }
+
     return middleware[0]({
         ...context,
         next: middlewarePipeline(context, middleware, 1),
